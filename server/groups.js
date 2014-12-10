5b4a75da339df7e0b7ee14116a5ad2b5b2fd2f4c
Albin Svensson <albin.svensson@trialbee.com>
2014-12-10 20:27:23 +0100

    Declare variables with var to keep them in scope

--- server/groups.js
+++ server/groups.js
@@ -30,8 +30,8 @@ getSubSquare = function(i){
   var row = (i%3);
   var col = (i/3) >> 0;
   var square = [];
-  for (y = col ; y < col + 2 ; y++) {
-    for (x = row ; x < row + 2 ; x++){
+  for (var y = col ; y < col + 2 ; y++) {
+    for (var x = row ; x < row + 2 ; x++){
       square.push(x+4*y);
     }
   }
@@ -44,15 +44,15 @@ getSquares = function(i){
   var col = (i/size) >> 0;
   var squares = [];
 
-  for (yy = col - 1 ; yy < col + 1 ; yy++) {
-    if (yy == - 1 || yy == size - 1) {
+  for (var y = col - 1 ; y < col + 1 ; y++) {
+    if (y == - 1 || y == size - 1) {
       continue;
     }
-    for (xx = row - 1 ; xx < row + 1 ; xx++) {
-      if (xx == -1 || xx == size - 1) {
+    for (var x = row - 1 ; x < row + 1 ; x++) {
+      if (x == -1 || x == size - 1) {
         continue;
       }
-      squares.push(getSubSquare((xx+3*yy)));
+      squares.push(getSubSquare((x+3*y)));
     }
   }
   return squares;
